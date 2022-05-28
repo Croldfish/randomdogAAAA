65aca5bfd2dbdace99c1f78fc440201c7c032e07
Croldfish <70867293+Croldfish@users.noreply.github.com>
2022-05-27 22:14:55 -0400

    an installer script
    MEANT FOR MACOS NOT RECOOMENDED FOR LINUX AS IT INSTALLS BREW AND I'VE HAD ISSUES WITH IT IN THE PAST ON FEDORA LINUX

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+exec /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+exec brew install python3 wget
+
+exec pip install dog pillow
+
+exec wget https://github.com/Croldfish/randomdogAAAA/blob/23df6915067fa017a76a1cf4da2298ea1bb4a123/dog.py
+
+exec python3 dog.py 
